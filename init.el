2453923f3c1796cc05f68952dc96029a60c8265a
Bertrand Lallau <bertrand.lallau@gmail.com>
2015-10-27 16:42:55 +0100

    Correct init.el file

--- init.el
+++ init.el
@@ -1,21 +1,26 @@
 ;; Emacs Config
 
+;; Add lisp directory to path
+;(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(eval-and-compile
+  (require 'cask "~/.cask/cask.el")
+  (cask-initialize)
+  (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+  )
+
+(package-initialize)
+
 ;; Recompile .emacs.d/lisp & .emacs.d/lisp-config
 (byte-recompile-directory "~/.emacs.d/lisp" 0)
 (byte-recompile-directory "~/.emacs.d/lisp-config" 0)
-
-;; Add lisp directory to path
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
-(package-initialize)
-(require 'cask "~/.cask/cask.el")
 
 (load-library "proxy")
 (load-library "pre-cask")
 (load-library "customize")
 
 ;; Init All Plugin with Cask & Pallet
-(cask-initialize)
+;(cask-initialize)
 
 ;; Load all files with *.elc in lisp-config
 (mapc 'load-library (directory-files (expand-file-name "lisp-config" user-emacs-directory) t ".elc$"))
