44601fe67df05876e790295302b91abbacd85ebf
Bertrand Lallau <bertrand.lallau@gmail.com>
2015-10-27 10:48:59 +0100

    Add git-cutter config

--- lisp-config/git-gutter.el
+++ lisp-config/git-gutter.el
@@ -1,4 +1,10 @@
+(custom-set-variables
+ '(git-gutter:handled-backends '(git)))
 (global-git-gutter-mode +1)
+
+(custom-set-variables
+ '(git-gutter:hide-gutter t))
+;(add-hook 'python-mode-hook 'git-gutter-mode)
 
 ;; Jump to next/previous hunk
 (global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
