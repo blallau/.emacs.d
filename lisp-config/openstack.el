a28ae3f7d4d4cd78508501826927d69f8a8ff4f7
Bertrand Lallau <bertrand.lallau@gmail.com>
2015-10-27 11:06:12 +0100

    Add copyright to Openstack launchpad/review browse-url

--- lisp-config/openstack.el
+++ lisp-config/openstack.el
@@ -1,3 +1,23 @@
+;; Copyright © 2015 Bertrand Lallau
+
+;; Author: Bertrand LALLAU <bertrand.lallau@gmail.com>
+;; URL: https://github.com/blallau/emacs-config
+;; Package-Version: 1.0.0
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses/.
+
+
 (defvar launchpad-url "https://bugs.launchpad.net/" "launchpad URL")
 (defvar openstack-review-url "https://review.openstack.org/#q," "Openstack review URL")
 
@@ -9,11 +29,9 @@
           (case-fold-search t))
       (cond
        ((string-match "[[:alpha:]]+-Bug:[[:space:]]*#\\([[:digit:]]+\\)" identifiant)
-;;      (message (concat "ID extract=" (match-string 1 identifiant)))
         (browse-url (concat launchpad-url project-name "/+bug/" (match-string 1 identifiant)))
         )
        ((string-match "Change-Id: \\([[:alnum:]]+\\)" identifiant)
-        ;;      (message (concat "ID extract=" (match-string 1 identifiant)))
         (browse-url (concat openstack-review-url (match-string 1 identifiant) ",n,z"))
         )
        (t
