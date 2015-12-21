2b92a86a278dca7068311235d360c0eeaf5d90bf
Bertrand Lallau <bertrand.lallau@gmail.com>
2015-12-21 16:33:12 +0100

    indent defadvice code

--- lisp/customize.el
+++ lisp/customize.el
@@ -46,18 +46,18 @@
 ;; Next and previous buffer ignore *...* buffer
 ;;
 (defadvice next-buffer (after avoid-messages-buffer-in-next-buffer)
-"Advice around `next-buffer' to avoid going into the *Messages* buffer."
-;;(when (string= "*Messages*" (buffer-name))
-(when (string-match "\\*.*\\*" (buffer-name))
-(next-buffer)))
+  "Advice around `next-buffer' to avoid going into the *Messages* buffer."
+  ;;(when (string= "*Messages*" (buffer-name))
+  (when (string-match "\\*.*\\*" (buffer-name))
+    (next-buffer)))
 ;; activate the advice
 (ad-activate 'next-buffer)
-(defadvice previous-buffer (after avoid-messages-buffer-in-next-buffer)
-"Advice around `previous-buffer' to avoid going into the *Messages* buffer."
-;;(when (string= "*Messages*" (buffer-name))
-(when (string-match "\\*.*\\*" (buffer-name))
-  (previous-buffer)))
 
+(defadvice previous-buffer (after avoid-messages-buffer-in-next-buffer)
+  "Advice around `previous-buffer' to avoid going into the *Messages* buffer."
+  ;;(when (string= "*Messages*" (buffer-name))
+  (when (string-match "\\*.*\\*" (buffer-name))
+    (previous-buffer)))
 ;; activate the advice
 (ad-activate 'previous-buffer)
 
