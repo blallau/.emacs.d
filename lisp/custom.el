82a2ace209162279205642ecace309adf96114c3
Bertrand Lallau <bertrand.lallau@gmail.com>
2015-12-23 12:00:28 +0100

    Disable elpy highlight mode

--- lisp/custom.el
+++ lisp/custom.el
@@ -6,6 +6,7 @@
  '(custom-safe-themes
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(elpy-mode-hook nil)
  '(expand-region-guess-python-mode nil)
  '(expand-region-preferred-python-mode (quote python))
  '(magit-diff-use-overlays nil)
