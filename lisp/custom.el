66f295777c36ea127da9fb85349d4cc2a4a46fcb
Bertrand Lallau <bertrand.lallau@gmail.com>
2015-12-30 10:44:55 +0100

    Cleanup custom.el

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,15 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-diff-use-overlays nil)
  '(magit-log-arguments
    (quote
     ("--graph" "--color" "--decorate" "++header" "--no-merges" "-n256")))
 ; '(magit-log-section-arguments (quote ("--decorate")))
 ; '(magit-log-select-arguments (quote ("-n256" "--decorate")))
- '(magit-use-overlays nil)
- '(python-indent-guess-indent-offset nil)
- '(sp-autoescape-string-quote nil))
+ )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
