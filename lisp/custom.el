4d7c3d602513f38a371294c9f87bf0661a498969
Bertrand Lallau <bertrand.lallau@gmail.com>
2015-12-30 10:35:50 +0100

    Remove custom-safe-themes from custom.el

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,9 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(expand-region-guess-python-mode nil)
  '(expand-region-preferred-python-mode (quote python))
  '(magit-diff-use-overlays nil)
