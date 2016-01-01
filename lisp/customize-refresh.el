d389ca1d93a90b6afc10dd42e65357cd26ec7fe9
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-01-01 13:53:10 +0100

    small fix

--- lisp/customize-refresh.el
+++ lisp/customize-refresh.el
@@ -1,7 +1,7 @@
+(require 'autorevert)
 ;; Auto refresh buffers
 (global-auto-revert-mode 1)
 
 ;; Also auto refresh dired, but be quiet about it
-(require 'autorevert)
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
