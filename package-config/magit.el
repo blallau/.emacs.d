509b03e53520dd1c10fd787351c29b58659d1adc
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-01-01 11:20:53 +0100

    Fully refresh magit config

--- package-config/magit.el
+++ package-config/magit.el
@@ -1,9 +1,10 @@
 (require 'magit)
 
-(defvar pc:magit-from-buffer
-  "Buffer from where magit-status were last called."
-  nil)
+;; Configure git-commit
+(setq git-commit-summary-max-length 50)
+(setq git-commit-fill-column 72)
 
+;; Configure Magit
 ;; Disable the `highlight` face that Magit uses to highlight diffs. It's unreadable with my color scheme.
 ;; An unreadable highlight face is a common issue on the Magit tracker.
 (defun disable-magit-highlight-in-buffer () (face-remap-add-relative 'magit-item-highlight '()))
@@ -11,10 +12,9 @@
 (add-hook 'magit-commit-mode-hook 'disable-magit-highlight-in-buffer)
 (add-hook 'magit-diff-mode-hook 'disable-magit-highlight-in-buffer)
 
-;; Configure Magit
-(setq git-commit-summary-max-length 50)
-(setq git-commit-fill-column 72)
 (setq magit-log-arguments (quote ("--graph" "--color" "--decorate" "++header" "--no-merges" "-n256")))
+(setq magit-revert-buffers t)
+(setq magit-save-repository-buffers t)
 
 ;(global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-<f5>") 'magit-blame)
