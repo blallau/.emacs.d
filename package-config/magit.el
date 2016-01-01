305f325861a1667c737fdc5d66c18120c14ef03d
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-01-01 10:33:27 +0100

    Update Magit configuration * remove deleted functions and variables
    Remove custom.el file

--- package-config/magit.el
+++ package-config/magit.el
@@ -4,16 +4,6 @@
   "Buffer from where magit-status were last called."
   nil)
 
-(defun pc:magit-status-buffer-switch (buf)
-  "My replacement for `magit-status-buffer-switch-function'.
-`magit-status' does not split windows (switch to magit buffer
-instead). Also store the current buffer to switch back to it when
-quitting.
-TODO: store the whole frame config instead?"
-  (setq pc:magit-from-buffer (current-buffer))
-  (switch-to-buffer buf))
-(setq magit-status-buffer-switch-function 'pc:magit-status-buffer-switch)
-
 ;; Disable the `highlight` face that Magit uses to highlight diffs. It's unreadable with my color scheme.
 ;; An unreadable highlight face is a common issue on the Magit tracker.
 (defun disable-magit-highlight-in-buffer () (face-remap-add-relative 'magit-item-highlight '()))
@@ -24,15 +14,7 @@ TODO: store the whole frame config instead?"
 ;; Configure Magit
 (setq git-commit-summary-max-length 50)
 (setq git-commit-fill-column 72)
-
-(setq magit-commit-signoff t)
-(setq magit-remote-ref-format 'remote-slash-branch)
-;(setq magit-completing-read-function 'magit-iswitchb-completing-read)
-(setq magit-save-some-buffers nil)
-
-; magit 1.4.0 (no revert before git)
-(setq magit-auto-revert-mode t)
-(setq magit-last-seen-setup-instructions "1.4.0")
+(setq magit-log-arguments (quote ("--graph" "--color" "--decorate" "++header" "--no-merges" "-n256")))
 
 ;(global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-<f5>") 'magit-blame)
