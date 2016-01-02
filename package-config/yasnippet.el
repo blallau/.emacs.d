19b26f2b83a2d8dc7d302c380daa8612d2a7fa10
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-01-02 09:50:53 +0100

    Yasnippet enhancement

---
+++ package-config/yasnippet.el
@@ -0,0 +1,15 @@
+(require 'yasnippet)
+
+(yas-reload-all)
+(add-hook 'python-mode-hook #'yas-minor-mode)
+
+(defun yas--magit-email-or-default ()
+  "Get email from GIT or use default"
+  (if (magit-toplevel ".")
+      (magit-get "user.email")
+    user-mail-address))
+
+(defun yas--magit-user-name ()
+  "Get user-name from GIT"
+  (when (magit-toplevel ".")
+    (magit-get "user.name")))
