32e0490a112313a9c954c2601e06121b933417f8
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-01-06 16:44:21 +0100

    Set recentf location and max values

--- lisp/customize-init.el
+++ lisp/customize-init.el
@@ -22,7 +22,9 @@
 (require 'recentf)
 (setq recentf-auto-cleanup 'never) ;; disable before we start recentf!
 (recentf-mode 1)
-(setq recentf-max-saved-items 1000
+(setq recentf-max-saved-items 200
+      recentf-max-menu-items 15
+      recentf-save-file (expand-file-name ".recentf" user-emacs-directory)
       recentf-exclude '("/tmp/" "/ssh:"))
 
 (add-to-list 'auto-mode-alist
