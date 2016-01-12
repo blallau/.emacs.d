61bb16dc894a2f2bb10b0433f5d818a15dd55805
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-01-12 17:58:55 +0100

    Disable yasnippet actually not used

--- package-config/yasnippet.el
+++ package-config/yasnippet.el
@@ -1,15 +1,15 @@
-(require 'yasnippet)
+;; (require 'yasnippet)
 
-(yas-reload-all)
-(add-hook 'python-mode-hook #'yas-minor-mode)
+;; (yas-reload-all)
+;; (add-hook 'python-mode-hook #'yas-minor-mode)
 
-(defun yas--magit-email-or-default ()
-  "Get email from GIT or use default"
-  (if (magit-toplevel ".")
-      (magit-get "user.email")
-    user-mail-address))
+;; (defun yas--magit-email-or-default ()
+;;   "Get email from GIT or use default"
+;;   (if (magit-toplevel ".")
+;;       (magit-get "user.email")
+;;     user-mail-address))
 
-(defun yas--magit-user-name ()
-  "Get user-name from GIT"
-  (when (magit-toplevel ".")
-    (magit-get "user.name")))
+;; (defun yas--magit-user-name ()
+;;   "Get user-name from GIT"
+;;   (when (magit-toplevel ".")
+;;     (magit-get "user.name")))
