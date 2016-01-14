d11680d7dccd2857a9c92a2272a63d25dfaf65c3
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-01-14 21:18:47 +0100

    Fix magit-gerrit conf

--- package-config/magit-gerrit.el
+++ package-config/magit-gerrit.el
@@ -1,2 +1,8 @@
 (require 'magit-gerrit)
-(setq-default magit-gerrit-ssh-creds "blallau@gerrithost.org")
+
+;; if remote url is not using the default gerrit port and
+;; ssh scheme, need to manually set this variable
+;;(setq-default magit-gerrit-ssh-creds "myid@gerrithost.org")
+
+;; if necessary, use an alternative remote instead of 'origin'
+(setq-default magit-gerrit-remote "gerrit")
