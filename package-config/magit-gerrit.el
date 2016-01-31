2f7ea72861265305eda8cf97e2dfc2f947125e17
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-01-31 22:29:04 +0100

    Remove magit-gerrit plugin

--- package-config/magit-gerrit.el
+++
@@ -1,8 +0,0 @@
-(require 'magit-gerrit)
-
-;; if remote url is not using the default gerrit port and
-;; ssh scheme, need to manually set this variable
-;;(setq-default magit-gerrit-ssh-creds "myid@gerrithost.org")
-
-;; if necessary, use an alternative remote instead of 'origin'
-(setq-default magit-gerrit-remote "gerrit")
