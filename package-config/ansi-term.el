0a232180b70e08ca50ec3baeabdbb757b1de8964
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-02-01 17:36:35 +0100

    Configure ansi-term

--- package-config/ansi-term.el
+++ package-config/ansi-term.el
@@ -9,3 +9,43 @@
     (switch-to-buffer-other-window "*ansi-term*")))
 
 (global-set-key (kbd "C-c t") 'visit-term-buffer)
+
+(add-hook 'term-mode-hook
+	  (lambda ()
+	    (setq header-line-format nil)
+	    (setq mode-line-format nil)))
+
+;; Exit in terminal kill buffer
+(defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
+  (if (memq (process-status proc) '(signal exit))
+      (let ((buffer (process-buffer proc)))
+        ad-do-it
+        (kill-buffer buffer))
+    ad-do-it))
+(ad-activate 'term-sentinel)
+
+;; Don't ask me which shell to use every time
+(defvar my-term-shell "/bin/bash")
+(defadvice ansi-term (before force-bash)
+  (interactive (list my-term-shell)))
+(ad-activate 'ansi-term)
+
+;; Issue displaying of certain characters and control codes
+(defun my-term-use-utf8 ()
+  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+(add-hook 'term-exec-hook 'my-term-use-utf8)
+
+;; clickable URLs
+(defun my-term-hook ()
+  (goto-address-mode))
+(add-hook 'term-mode-hook 'my-term-hook)
+
+(defun my-term-paste (&optional string)
+  (interactive)
+  (process-send-string
+   (get-buffer-process (current-buffer))
+   (if string string (current-kill 0))))
+
+(defun my-term-hook ()
+  (goto-address-mode)
+  (define-key term-raw-map "\C-y" 'my-term-paste))
