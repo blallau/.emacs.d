b0ab697227b7aaa305be6eab0d1ff9fb6dc2fb3b
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-03-01 14:51:38 +0100

    Add puml-mode plugin

---
+++ package-config/puml-mode.el
@@ -0,0 +1,8 @@
+(require 'puml-mode)
+
+;; Enable puml-mode for PlantUML files
+(add-to-list 'auto-mode-alist '("\\.puml\\'" . puml-mode))
+(add-to-list 'auto-mode-alist '("\\.uml\\'" . puml-mode))
+(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . puml-mode))
+
+(setq puml-output-type "svg")
