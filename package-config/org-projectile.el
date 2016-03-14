e9964df76e03030dc8c45b37baebfedf2c5a6496
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-03-14 22:43:26 +0100

    Org-mode: add projectile extension Update Cask file

---
+++ package-config/org-projectile.el
@@ -0,0 +1,9 @@
+(use-package org-projectile
+  :bind (("C-c n p" . org-projectile:project-todo-completing-read)
+         ("C-c c" . org-capture))
+  :config
+  (progn
+    (setq org-projectile:projects-file "~/work/org/projects.org")
+    (setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
+    (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "p")))
+  :ensure t)
