3f5c2b2192cd51377a90e7bba5a08118069563d7
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-03-14 23:00:57 +0100

    Org-mode add projectile extension

--- package-config/org-projectile.el
+++ package-config/org-projectile.el
@@ -1,9 +1,8 @@
-(use-package org-projectile
-  :bind (("C-c n p" . org-projectile:project-todo-completing-read)
-         ("C-c c" . org-capture))
-  :config
-  (progn
-    (setq org-projectile:projects-file "~/work/org/projects.org")
-    (setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
-    (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "p")))
-  :ensure t)
+(require 'org-projectile)
+(setq org-projectile:projects-file "~/work/org/projects.org")
+(setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
+
+(add-to-list 'org-capture-templates (org-projectile:project-todo-entry))
+
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c n p") 'org-projectile:project-todo-completing-read)
