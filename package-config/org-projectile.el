4d20cea0c75e1024dd8d9f02bcdf88da82a8d135
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-03-15 21:52:16 +0100

    Org-mode projectile: disable prompt on switch project

--- package-config/org-projectile.el
+++ package-config/org-projectile.el
@@ -6,3 +6,5 @@
 
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c n p") 'org-projectile:project-todo-completing-read)
+;; disable prompt
+(setq org-confirm-elisp-link-function nil)
