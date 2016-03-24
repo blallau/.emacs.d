22c770dc2292f636d2b8b4575be75383c9a99fe6
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-03-24 17:54:10 +0100

    Org-mode: refactor configuration

--- package-config/org-mode.el
+++ package-config/org-mode.el
@@ -1,29 +1,73 @@
 (require 'org)
 
-;; Hook to update all blocks before saving
+;; Org directory config
+(setq org-directory "~/org"
+      org-default-notes-file (concat org-directory "/notes.org")
+      ;; all files to include in order to compose agenda
+      org-agenda-files (list (concat org-directory "/gtd.org")
+                             (concat org-directory "/notes.org")))
+
+;;
+;; Hooks
+;;
+;; to update all blocks before saving
 (add-hook 'org-mode-hook
           (lambda() (add-hook 'before-save-hook
                               'org-update-all-dblocks t t)))
-
-(setq org-startup-indented t)
-(setq org-hide-leading-stars nil)
-
-;; render text with delimiters
-(setq org-hide-emphasis-markers t)
-
-;; don't want links to be explicit
-(setq org-descriptive-links t)
+(add-hook 'org-mode-hook 'org-display-inline-images)
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
 
 ;;
-;; Tags
+;; Appearance
 ;;
-(setq org-group-tags t
-      org-tag-alist '((:startgroup . nil)
-                      ("@work" . ?w) ("@home" . ?h)
-                      (:endgroup . nil)))
+(setq org-startup-indented t
+      org-hide-leading-stars nil
+      ;; render text with delimiters
+      org-hide-emphasis-markers t
+      ;; leave a blank line at the end of the content of each task entry
+      org-blank-before-new-entry (quote ((heading) (plain-list-item)))
+      ;; set maximum indentation for description lists
+      org-list-description-max-indent 5
+      ;; %25ITEM %TODO %3PRIORITY %TAGS
+      org-columns-default-format "%25ITEM(Task) %TODO(State) %3PRIORITY %TAGS(Tags)"
+      ;; prevent demoting heading also shifting text inside sections
+      org-adapt-indentation nil
+
+      ;;
+      ;; Table
+      ;;
+      ;; only work with the internal format (like @3$2 or $4)
+      org-table-use-standard-references nil
+
+      ;;
+      ;; Links
+      ;;
+      ;; With the following settings, you could link to a specific bug with
+      ;; `[[jira:1234]]'
+      ;; `[[lp:14134]]'
+      ;; `[[review:14134/1]]'
+      org-link-abbrev-alist
+      '(("jira" . "http://pegjira.pegasus.theresis.org/browse/")
+        ("lp" . "https://bugs.launchpad.net/bugs/")
+        ("review" . "https://review.openstack.org/#/c/"))
+      ;; follow the link at point with <RET>
+      org-return-follows-link t
+      ;; don't want links to be explicit
+      org-descriptive-links t
+
+      ;;
+      ;; Footnode
+      ;;
+      org-footnote-section nil
+      org-footnote-auto-adjust t)
+
 
 ;; Information to record when a task moves to the DONE state
 (setq org-log-done 'time
+
+      ;; forces to mark all child tasks as “DONE” before you can mark the parent as “DONE”
+      org-enforce-todo-dependencies t
+
       ;; Enable task switching shortcuts
       org-use-fast-todo-selection t
       org-todo-keywords '((sequence "☛ TODO(t)" "➱ IN-PROGRESS(i)" "⚑ WAITING(w)" "|" "✔ DONE(d)" "✘ CANCELED(c)")
@@ -42,14 +86,14 @@
                                ("✘ NOT FIXED" . (:background "#d7ff5f" :foreground "#008700" :weight bold
                                                              :strike-through t
                                                              :box (:line-width 1 :color nil :style none)))))
-
-;; Org directory config
-(setq org-directory "~/org"
-      org-default-notes-file (concat org-directory "/notes.org")
-      ;; all files to include in order to compose agenda
-      org-agenda-files (list (concat org-directory "/gtd.org")
-                             (concat org-directory "/notes.org")))
-
+;;
+;; Tags
+;;
+(setq org-group-tags t
+      org-tags-exclude-from-inheritance '("prj")
+      org-tag-alist '((:startgroup . nil)
+                      ("@work" . ?w) ("@home" . ?h)
+                      (:endgroup . nil)))
 ;;
 ;; Priorities
 ;;
@@ -61,17 +105,6 @@
       org-priority-faces '((?A . (:foreground "#F0DFAF" :weight bold))
                            (?B . (:foreground "LightSteelBlue"))
                            (?C . (:foreground "OliveDrab"))))
-
-;; %25ITEM %TODO %3PRIORITY %TAGS
-(setq org-columns-default-format "%25ITEM(Task) %TODO(State) %3PRIORITY %TAGS(Tags)")
-
-;;
-;; Todo
-;;
-
-;; forces to mark all child tasks as “DONE” before you can mark the parent as “DONE”
-(setq org-enforce-todo-dependencies t)
-
 ;;
 ;; Capture
 ;;
@@ -82,22 +115,6 @@
         ("l" "Link" entry
          (file+headline "links.org" "Links to Read")
          "* %?\n %i")))
-
-;; leave a blank line at the end of the content of each task entry
-(setq org-blank-before-new-entry (quote ((heading) (plain-list-item))))
-
-;; set maximum indentation for description lists
-(setq org-list-description-max-indent 5)
-
-;; prevent demoting heading also shifting text inside sections
-(setq org-adapt-indentation nil)
-
-;;
-;; Footnode
-;;
-(setq org-footnote-section nil
-      org-footnote-auto-adjust t)
-
 ;;
 ;; Agenda
 ;;
@@ -136,6 +153,25 @@
 
       org-agenda-skip-archived-trees nil
 
+      org-agenda-category-icon-alist
+      '((".[Ee]macs.*" "/usr/share/icons/hicolor/16x16/apps/emacs.png" nil nil :ascent center)
+        ("[Oo]rg" "~/.emacs.d/icons/org/org.png" nil nil :ascent center)
+        ("[Tr]rip" "~/.emacs.d/icons/org/trip.png" nil nil :ascent center)
+        ("[Aa]nniv" "~/.emacs.d/icons/org/anniversary.png" nil nil :ascent center)
+        ("\\([Hh]olidays\\|[Vv]acation\\)" "~/.emacs.d/icons/org/holidays.png" nil nil :ascent center)
+        (".*cinder" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
+        ("designate" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
+        ("glance" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
+        ("heat" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
+        ("keystone" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
+        (".*neutron" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
+        (".*nova" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
+        ("octavia" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
+        (".*" '(space . (:width (16)))))
+
+      ;; How to identify stuck projects
+      org-stuck-projects '("+prj/-✔ DONE-✘ CANCELED-✔ FIXED-✘ NOT FIXED"
+                           ("☛ TODO" "☠ BUG") nil "")
       ;;
       ;; Agenda/Todo
 
@@ -150,53 +186,9 @@
                                     (todo priority-down category-keep)
                                     (tags priority-down category-keep)
                                     (search category-keep))))
-
-(setq org-agenda-category-icon-alist
-      '((".[Ee]macs.*" "/usr/share/icons/hicolor/16x16/apps/emacs.png" nil nil :ascent center)
-        ("[Oo]rg" "~/.emacs.d/icons/org/org.png" nil nil :ascent center)
-        ("[Tr]rip" "~/.emacs.d/icons/org/trip.png" nil nil :ascent center)
-        ("[Aa]nniv" "~/.emacs.d/icons/org/anniversary.png" nil nil :ascent center)
-        ("\\([Hh]olidays\\|[Vv]acation\\)" "~/.emacs.d/icons/org/holidays.png" nil nil :ascent center)
-        (".*cinder" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
-        ("designate" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
-        ("glance" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
-        ("heat" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
-        ("keystone" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
-        (".*neutron" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
-        (".*nova" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
-        ("octavia" "~/.emacs.d/icons/org/openstack.png" nil nil :ascent center)
-        (".*" '(space . (:width (16))))))
-
-;; How to identify stuck projects
-(setq org-tags-exclude-from-inheritance '("prj")
-      org-stuck-projects '("+prj/-✔ DONE-✘ CANCELED-✔ FIXED-✘ NOT FIXED"
-                           ("☛ TODO" "☠ BUG") nil ""))
-
 ;;
-;; Table
-;;
-;; only work with the internal format (like @3$2 or $4)
-(setq org-table-use-standard-references nil)
-
-;; Links
-;;
-;; follow the link at point with <RET>
-(setq org-return-follows-link t)
-
-;; With the following settings, you could link to a specific bug with
-;; `[[jira:1234]]'
-;; `[[lp:14134]]'
-;; `[[review:14134/1]]'
-(setq org-link-abbrev-alist
-      '(("jira" . "http://pegjira.pegasus.theresis.org/browse/")
-        ("lp" . "https://bugs.launchpad.net/bugs/")
-        ("review" . "https://review.openstack.org/#/c/")))
-
 ;; Source code block
 ;;
-;; Don't ask before every code block evaluation
-(setq org-confirm-babel-evaluate nil)
-
 ;; syntax highlight code blocks
 (setq org-src-fontify-natively t)
 (setq org-src-preserve-indentation t)
@@ -211,15 +203,14 @@
     (org-redisplay-inline-images)))
 
 ;; permit inline image display in the Emacs buffer.
-(add-hook 'org-mode-hook 'org-display-inline-images)
 (add-hook 'org-babel-after-execute-hook 'shk-fix-inline-images)
-
-;; turn on auto-fill
-(add-hook 'org-mode-hook 'turn-on-auto-fill)
 
 ;;
 ;; Babel
 ;;
+;; Don't ask before every code block evaluation
+(setq org-confirm-babel-evaluate nil)
+
 ;; Some initial languages we want org-babel to support
 (org-babel-do-load-languages
  'org-babel-load-languages
