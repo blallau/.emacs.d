5ba8fbe68575cb63f97800482926ec1ccf88c25d
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-03-31 11:04:41 +0200

    Golang: refactor config

--- package-config/golang.el
+++ package-config/golang.el
@@ -1,9 +1,17 @@
-(require 'go-mode)
-(require 'go-projectile)
-
 ;; go get -u github.com/nsf/gocode
 ;; go get -u github.com/rogpeppe/godef
 ;; go get golang.org/x/tools/cmd/oracle
+
+(require 'go-mode)
+(require 'go-projectile)
+
+(eval-after-load 'go-mode
+  '(progn
+    ;; Set $GOPATH
+    (go-projectile-set-gopath)
+    ;; Set $PATH to $PATH:~/.emacs.d/gotools/bin
+    (go-projectile-tools-add-path)))
+
 
 (add-hook 'before-save-hook 'gofmt-before-save)
 
@@ -17,13 +25,16 @@
                            (local-set-key (kbd "C-c C-k") 'godoc)))
 
 ;; eldoc
-(defun go-mode-setup ()
-  (go-eldoc-setup))
+(add-hook 'go-mode-hook 'go-eldoc-setup)
 
-(add-hook 'go-mode-hook 'go-mode-setup)
+;; oracle
+(defvar oracle-file (concat go-projectile-tools-path "/src/"
+                            (cdr (assq 'oracle go-projectile-tools))
+                            "/oracle.el"))
+(if (file-exists-p oracle-file)
+    (load-file oracle-file))
 
-;;(load "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
-;;(add-hook 'go-mode-hook 'go-oracle-mode)
+;; (add-hook 'go-mode-hook 'go-oracle-mode)
 
 ;; autocomplete
 ;;
