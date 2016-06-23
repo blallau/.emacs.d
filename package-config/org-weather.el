59ed13360fcc66710668033f088f87026c80315a
Bertrand Lallau <bertrand.lallau@gmail.com>
2016-06-23 22:47:47 +0200

    org-weather full config

--- package-config/org-weather.el
+++ package-config/org-weather.el
@@ -1,6 +1,7 @@
 ;; Load the org-weather library
 (require 'org-weather)
 ;; Set your location and refresh the data
-(setq org-weather-location "Paris,FR"
-      org-weather-format "Weather: %desc, %tmin-%tmax%tu, %p%pu, %h%hu, %s%su")
+(setq org-weather-location "Jouy-en-josas,FR"
+      org-weather-format "Weather: %desc, %tmin-%tmax%tu, %h%hu, %s%su"
+      org-weather-api-key "8b0a4c33ea005be33324f6aeb8120991")
 (org-weather-refresh)
