d7ddc2958fc9624ad0e267993fd89ab01d3f412a
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-06-28 17:35:48 +0200

    magit "use-package" ready

--- package-config/magit.el
+++ package-config/magit.el
@@ -1,26 +1,25 @@
-(eval-when-compile
-  (require 'magit))
+(use-package magit
+  :config
+  ;; Configure git-commit
+  (setq git-commit-summary-max-length 50
+        git-commit-fill-column 72)
 
-;; Configure git-commit
-(setq git-commit-summary-max-length 50)
-(setq git-commit-fill-column 72)
+  ;; Configure Magit
+  ;; Disable the `highlight` face that Magit uses to highlight diffs. It's unreadable with my color scheme.
+  ;; An unreadable highlight face is a common issue on the Magit tracker.
+  (defun disable-magit-highlight-in-buffer () (face-remap-add-relative 'magit-item-highlight '()))
+  (add-hook 'magit-status-mode-hook 'disable-magit-highlight-in-buffer)
+  (add-hook 'magit-commit-mode-hook 'disable-magit-highlight-in-buffer)
+  (add-hook 'magit-diff-mode-hook 'disable-magit-highlight-in-buffer)
 
-;; Configure Magit
-;; Disable the `highlight` face that Magit uses to highlight diffs. It's unreadable with my color scheme.
-;; An unreadable highlight face is a common issue on the Magit tracker.
-(defun disable-magit-highlight-in-buffer () (face-remap-add-relative 'magit-item-highlight '()))
-(add-hook 'magit-status-mode-hook 'disable-magit-highlight-in-buffer)
-(add-hook 'magit-commit-mode-hook 'disable-magit-highlight-in-buffer)
-(add-hook 'magit-diff-mode-hook 'disable-magit-highlight-in-buffer)
+  (setq magit-log-arguments (quote ("--graph" "--color" "--decorate" "++header" "--no-merges" "-n256"))
+        magit-revert-buffers t
+        magit-save-repository-buffers t)
 
-(setq magit-log-arguments (quote ("--graph" "--color" "--decorate" "++header" "--no-merges" "-n256")))
-(setq magit-revert-buffers t)
-(setq magit-save-repository-buffers t)
+  ;; follow symlinks without querying user
+  (setq vc-follow-symlinks t)
 
-;; follow symlinks without querying user
-(setq vc-follow-symlinks t)
-
-;(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-<f5>") 'magit-blame)
-(global-set-key (kbd "<f6>") 'magit-status)
-(global-set-key (kbd "C-<f6>") 'magit-log-buffer-file)
+  :bind
+  (("C-<f5>" . magit-blame)
+   ("<f6>" . magit-status)
+   ("C-<f6>" . magit-log-buffer-file)))
