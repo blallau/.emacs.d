3a3634d69d6e6eb892711359005995daf853a88f
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-06-29 12:14:02 +0200

    Ruby "use-package" ready

--- package-config/ruby.el
+++ package-config/ruby.el
@@ -1,45 +1,44 @@
-(require 'ruby-mode)
+(use-package ruby-mode
+  :defer t
+  :mode
+  (("\\.cap\\'" . ruby-mode)
+   ("\\.erb\\'" . ruby-mode)
+   ("\\.gemspec\\'" . ruby-mode)
+   ("\\.gemrc\\'" . ruby-mode)
+   ("\\.irbrc\\'" . ruby-mode)
+   ("\\.jbuilder\\'" . ruby-mode)
+   ("\\.rake\\'" . ruby-mode)
+   ("\\.rb\\'" . ruby-mode)
+   ("\\.ru\\'" . ruby-mode)
+   ("\\.thor\\'" . ruby-mode)
+   ("Brewfile\\'" . ruby-mode)
+   ("Capfile\\'" . ruby-mode)
+   ("Gemfile\\'" . ruby-mode)
+   ("Guardfile\\'" . ruby-mode)
+   ("[rR]akefile\\'" . ruby-mode)
+   ("Thorfile\\'" . ruby-mode)
+   ("Vagrantfile\\'" . ruby-mode))
 
-(defun ome-ruby-mode-setup ()
-  ;; Ah, this huge auto-mode-alist list comes from emacs prelude
-  (add-to-list 'auto-mode-alist '("\\.cap\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.gemrc\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.irbrc\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.thor\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Brewfile\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Capfile\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("[rR]akefile\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Thorfile\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Vagrantfile\\'" . ruby-mode)))
-(ome-ruby-mode-setup)
+  :bind
+  (("<f3>" . robe-jump)
+   ("<f2>" . pop-tag-mark)
+   ("<f1>" . robe-doc))
 
-(require 'flymake-ruby)
-(add-hook 'ruby-mode-hook #'flymake-ruby-load)
+  :config
+  (setq ruby-indent-level 2
+        ruby-indent-tabs-mode nil)
 
-;; ROBE: code assistance tool that uses a Ruby REPL subprocess
-;; with your application or gem code loaded, to provide information
-;; about loaded classes and modules, and where each method is defined.
-(add-hook 'ruby-mode-hook #'robe-mode)
-;; auto-complete
-(add-hook 'robe-mode-hook #'ac-robe-setup)
-;; M-x start-robe
+  (use-package flymake-ruby
+    :config
+    (add-hook 'ruby-mode-hook #'flymake-ruby-load))
 
-(when (require 'smartparens nil 'noerror)
-  (require 'smartparens-ruby))
+  ;; ROBE: code assistance tool that uses a Ruby REPL subprocess
+  ;; with your application or gem code loaded, to provide information
+  ;; about loaded classes and modules, and where each method is defined.
+  (add-hook 'ruby-mode-hook #'robe-mode)
+  ;; auto-complete
+  (add-hook 'robe-mode-hook #'ac-robe-setup)
+  ;; M-x start-robe
 
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (local-set-key (kbd "<f3>") 'robe-jump)
-            (local-set-key (kbd "<f2>") 'pop-tag-mark)
-            (local-set-key (kbd "<f1>") 'robe-doc)))
-
-(setq ruby-indent-level 2
-      ruby-indent-tabs-mode nil)
+  (when (require 'smartparens nil 'noerror)
+    (require 'smartparens-ruby)))
