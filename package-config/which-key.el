4cb3829f6659de667e8afdd059d8fc4178a98ac4
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-07-06 16:05:07 +0200

    which-key customization

--- package-config/which-key.el
+++ package-config/which-key.el
@@ -1,4 +1,6 @@
 (use-package which-key
   :config
   (which-key-mode)
-  (which-key-setup-side-window-right-bottom))
+  (which-key-setup-side-window-right-bottom)
+  (setq which-key-idle-delay 0.8
+        which-key-max-description-length 35))
