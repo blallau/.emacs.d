c2893798b26a57df6603a1088a3ceb37cc3fe90f
blallau <bertrand.lallau@gmail.com>
2016-08-29 22:05:07 +0200

    disable org-weather

--- package-config/org-weather.el
+++ package-config/org-weather.el
@@ -1,6 +1,6 @@
-(use-package org-weather
-  :config
-  (setq org-weather-location "Jouy-en-josas,FR"
-        org-weather-format "Weather: %desc, %tmin-%tmax%tu, %h%hu, %s%su"
-        org-weather-api-key "8b0a4c33ea005be33324f6aeb8120991")
-  (org-weather-refresh))
+;; (use-package org-weather
+;;   :config
+;;   (setq org-weather-location "Jouy-en-josas,FR"
+;;         org-weather-format "Weather: %desc, %tmin-%tmax%tu, %h%hu, %s%su"
+;;         org-weather-api-key "8b0a4c33ea005be33324f6aeb8120991")
+;;   (org-weather-refresh))
