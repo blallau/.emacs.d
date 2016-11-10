86a28855c1f7eeb09dad705a25c201f0731e07a9
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-11-10 13:42:50 +0100

    Add RST mode shortcuts

---
+++ package-config/rst-mode.el
@@ -0,0 +1,6 @@
+(defun my-keybindings-rst-hook ()
+  (local-set-key (kbd "<f3>") 'rst-forward-section)
+  (local-set-key (kbd "<f2>") 'rst-backward-section)
+  (local-set-key (kbd "<f1>") 'rst-toc))
+
+(add-hook 'rst-mode-hook #'my-keybindings-rst-hook)
