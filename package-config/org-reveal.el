c6a972a3cdb5f90944b0b3d78a44f704a34d0210
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2016-11-15 15:20:10 +0100

    org-reveal: fix env variable issue

--- package-config/org-reveal.el
+++ package-config/org-reveal.el
@@ -2,5 +2,5 @@
   :config
   ;; directory within which js/reveal.js is."
   ;; (setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/2.5.0/")
-  (setq org-reveal-root "~/work/pres/reveal.js/"
+  (setq org-reveal-root (substitute-in-file-name "$HOME/work/pres/reveal.js/")
         org-reveal-title-slide 'nil))
