271badc0e09ba07c7afeb2a7bf663991d861bc8b
Bertrand Lallau <bertrand.lallau@gmail.com>
2017-01-02 21:41:40 +0100

    Golden-ration package config enhancement

--- package-config/golden-ratio.el
+++ package-config/golden-ratio.el
@@ -1,5 +1,14 @@
 (use-package golden-ratio
+  :defer t
   :ensure t
+  :config
+  (setq golden-ratio-exclude-modes '(dired-mode
+                                     ediff-mode
+                                     erc-mode
+                                     flycheck-error-list-mode
+                                     )
+        golden-ratio-exclude-buffer-regexp
+        `(,(rx bos "*which-key*" eos)))
   :diminish golden-ratio-mode
   :init
   (golden-ratio-mode 1))
