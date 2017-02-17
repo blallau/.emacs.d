7443395fd856466da95e2298f6b7970eb5819588
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2017-02-17 14:31:58 +0100

    iedit limit its scope to current function

--- package-config/iedit.el
+++ package-config/iedit.el
@@ -1,2 +1,18 @@
 (use-package iedit
-  :bind ("C-;" . iedit-mode))
+  :config
+  (defun iedit-dwim (arg)
+    "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
+    (interactive "P")
+    (if arg
+        (iedit-mode)
+      (save-excursion
+        (save-restriction
+          (widen)
+          ;; this function determines the scope of `iedit-start'.
+          (if iedit-mode
+              (iedit-done)
+            ;; `current-word' can of course be replaced by other
+            ;; functions.
+            (narrow-to-defun)
+            (iedit-start (current-word) (point-min) (point-max)))))))
+  :bind ("C-;" . iedit-dwin))
