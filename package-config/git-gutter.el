ec18ce003f3bf042c651176c4d311308948ee71c
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2017-03-09 11:26:23 +0100

    Git-gutter: add C-f5 C-Sf5 shortkey

--- package-config/git-gutter.el
+++ package-config/git-gutter.el
@@ -3,6 +3,20 @@
   (setq git-gutter:handled-backends '(git)
         git-gutter:hide-gutter t)
   (global-git-gutter-mode +1)
+  :config
+  (defun git-gutter-reset-to-head-parent()
+    (interactive)
+    (let (parent (filename (buffer-file-name)))
+      (if (eq git-gutter:vcs-type 'svn)
+          (setq parent "PREV")
+        (setq parent (if filename (concat (shell-command-to-string (concat "git --no-pager log --oneline -n1 --pretty='format:%H' " filename)) "^") "HEAD^")))
+      (git-gutter:set-start-revision parent)
+      (message "git-gutter:set-start-revision HEAD^")))
+
+  (defun git-gutter-reset-to-default ()
+    (interactive)
+    (git-gutter:set-start-revision nil)
+    (message "git-gutter reset"))
   :bind
   (
    ;; Jump to next/previous hunk
@@ -11,4 +25,7 @@
    ;; Stage current hunk
    ("C-x v s" . git-gutter:stage-hunk)
    ;; Revert current hunk
-   ("C-x v r" . git-gutter:revert-hunk)))
+   ("C-x v r" . git-gutter:revert-hunk)
+   ;;
+   ("<C-f5>" . git-gutter-reset-to-head-parent)
+   ("<C-S-f5>" . git-gutter-reset-to-default)))
