89c4c25b12e8e5606ea4032eeaca2c2e7fc97680
Bertrand Lallau <bertrand.lallau@thalesgroup.com>
2017-03-09 15:15:21 +0100

    Modify git-gutter shortcuts
    <C-f2> git-gutter:previous-hunk
    <C-f3> git-gutter:next-hunk

--- package-config/git-gutter.el
+++ package-config/git-gutter.el
@@ -19,13 +19,15 @@
     (message "git-gutter reset"))
   :bind
   (
-   ;; Jump to next/previous hunk
-   ("C-x p" . git-gutter:previous-hunk)
-   ("C-x n" . git-gutter:next-hunk)
+   ;; ("C-x p" . git-gutter:previous-hunk)
+   ;; ("C-x n" . git-gutter:next-hunk)
    ;; Stage current hunk
    ("C-x v s" . git-gutter:stage-hunk)
    ;; Revert current hunk
    ("C-x v r" . git-gutter:revert-hunk)
    ;;
    ("<C-f5>" . git-gutter-reset-to-head-parent)
-   ("<C-S-f5>" . git-gutter-reset-to-default)))
+   ("<C-S-f5>" . git-gutter-reset-to-default)
+   ;; Jump to next/previous hunk
+   ("<C-f2>" . git-gutter:previous-hunk)
+   ("<C-f3>" . git-gutter:next-hunk)))
