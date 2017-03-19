5c455e2954bdecc49610fb9bb629af3a684e5b1b
Bertrand Lallau <bertrand.lallau@gmail.com>
2017-03-19 08:31:05 +0100

    Quickly edit files from dired mode

--- lisp/customize-edition.el
+++ lisp/customize-edition.el
@@ -71,3 +71,28 @@
     (kill-new (buffer-file-name (window-buffer (minibuffer-selected-window)))))
 
 (global-set-key (kbd "<C-S-f1>") 'my/filename)
+
+;; Quickly ediff files from dired
+;; press 'e' in dired-mode to immediately ediff two marked files
+(define-key dired-mode-map "e" 'ora-ediff-files)
+
+;; -*- lexical-binding: t -*-
+(defun ora-ediff-files ()
+  (interactive)
+  (let ((files (dired-get-marked-files))
+        (wnd (current-window-configuration)))
+    (if (<= (length files) 2)
+        (let ((file1 (car files))
+              (file2 (if (cdr files)
+                         (cadr files)
+                       (read-file-name
+                        "file: "
+                        (dired-dwim-target-directory)))))
+          (if (file-newer-than-file-p file1 file2)
+              (ediff-files file2 file1)
+            (ediff-files file1 file2))
+          (add-hook 'ediff-after-quit-hook-internal
+                    (lambda ()
+                      (setq ediff-after-quit-hook-internal nil)
+                      (set-window-configuration wnd))))
+      (error "no more than 2 files should be marked"))))
