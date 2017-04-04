2d66f67f9133ab7540684d661e9213b5126a835e
Bertrand Lallau <bertrand.lallau@gmail.com>
2017-04-04 07:38:39 +0200

    Add auto-revert on dired buffer

--- lisp/customize-refresh.el
+++ lisp/customize-refresh.el
@@ -5,3 +5,6 @@
 ;; Also auto refresh dired, but be quiet about it
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
+
+;; auto refresh dired when file changes
+(add-hook 'dired-mode-hook 'auto-revert-mode)
