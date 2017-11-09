cf3ac5d857dca34bfd4b9bff1ebaf38825e17b84
Bertrand Lallau <bertrand.lallau@gmail.com>
2017-11-09 09:06:33 +0100

    org-mode: add YouTube link type

--- package-config/org-mode.el
+++ package-config/org-mode.el
@@ -38,6 +38,28 @@
 ;;  (setq pdf-view-resize-factor 1.1)
 ;;  ;; keyboard shortcuts
 
+;; YouTube
+(defvar yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe width=\"440\""
+          " height=\"335\""
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
+
 ;; Org directory config
 (setq org-directory "~/org"
       org-default-notes-file (concat org-directory "/notes.org")
