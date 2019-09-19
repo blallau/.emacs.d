324fcc40e0175d0cf007fdcd0a49841903ee2988
T0125936 - LALLAU Bertrand <bertrand.lallau@thalesgroup.com>
2019-09-19 22:14:13 +0200

    Set Ivy and Counsel config

--- package-config/counsel.el
+++ package-config/counsel.el
@@ -1,13 +1,22 @@
 (use-package counsel :ensure t
   :bind*
   (("M-x"     . counsel-M-x)
+   ("C-s"     . swiper)
    ("C-x C-f" . counsel-find-file)
-   ("C-c f"   . counsel-git)
-   ("C-c s"   . counsel-git-grep)
-   ("C-c /"   . counsel-ag)
+   ("C-x C-r" . counsel-recentf)   ; search for recently edited
+   ("C-c c"   . counsel-compile)
+   ("C-c g"   . counsel-git)       ; search for files in git repo
+   ("C-c j"   . counsel-git-grep)  ; search for regexp in git repo
+   ("C-c /"   . counsel-ag)        ; Use ag for regexp
+   ("C-x l"   . counsel-locate)
+   ("<f1> f"  . counsel-describe-function)
+   ("<f1> v"  . counsel-describe-variable)
+   ("<f1> l"  . counsel-find-library)
+;   ("<f2> i"  . counsel-info-lookup-symbol)
+;   ("<f2> u"  . counsel-unicode-char)
+   ("C-c C-r" . ivy-resume)        ; Resume last Ivy-based completion
    ("C-c o"   . counsel-find-file-extern)
-   ("C-S-s"   . counsel-ag)
-   ("C-c l"   . counsel-locate))
+   )
   :config
   (setq counsel-find-file-at-point t)
   (setq counsel-locate-cmd 'counsel-locate-cmd-mdfind)
--- package-config/ivy.el
+++ package-config/ivy.el
@@ -1,4 +1,19 @@
 (use-package ivy :demand
+  :diminish (ivy-mode . "")
   :config
-  (setq ivy-use-virtual-buffers t
-        ivy-count-format "%d/%d "))
+  (ivy-mode 1)
+  (setq
+   ;; Add recent files and bookmarks to the ivy-switch-buffer
+   ivy-use-virtual-buffers 'recentf
+   ;; Displays the current and total number in the collection in prompt
+   ivy-count-format "%d/%d"
+   ;; number of result lines to display
+   ivy-height 12
+   ;; no regexp by default
+   ivy-initial-inputs-alist nil
+   )
+
+  (setq ivy-re-builders-alist
+        ;; allow input not in order
+        '((t   . ivy--regex-plus)))
+  )
