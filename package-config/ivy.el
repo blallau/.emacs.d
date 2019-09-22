f016d7732ad8d8f08ed7e085ca9784ce489ec57e
T0125936 - LALLAU Bertrand <bertrand.lallau@thalesgroup.com>
2019-09-22 14:58:25 +0200

    Ivy & Counsel factorization

--- package-config/ivy.el
+++ package-config/ivy.el
@@ -1,3 +1,5 @@
+;; IVY
+;;-----
 (use-package ivy :demand
   :diminish (ivy-mode . "")
   :config
@@ -17,3 +19,77 @@
         ;; allow input not in order
         '((t   . ivy--regex-plus)))
   )
+
+;; ivy-pass - Interface for pass
+;; https://github.com/ecraven/ivy-pass
+;;
+;; (use-package ivy-pass
+;;   :after ivy
+;;   :commands ivy-pass)
+
+;; ivy-rich - More friendly interface for ivy
+;; https://github.com/Yevgnen/ivy-rich
+;;
+(use-package ivy-rich
+  :after ivy
+  :config
+  ;; (defun ivy-rich-switch-buffer-icon (candidate)
+  ;;   (with-current-buffer
+  ;;  	(get-buffer candidate)
+  ;;     (let ((icon (all-the-icons-icon-for-mode major-mode)))
+  ;;   (if (symbolp icon)
+  ;;       (all-the-icons-icon-for-mode 'fundamental-mode)
+  ;;     icon))))
+  (setq ivy-rich-display-transformers-list
+      '(ivy-switch-buffer
+        (:columns
+         (
+          ;; (ivy-rich-switch-buffer-icon :width 2)
+          (ivy-rich-candidate (:width 30))
+          (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+          (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+          (ivy-rich-switch-buffer-project (:width 15 :face success))
+          (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+         :predicate
+         (lambda (cand) (get-buffer cand)))))
+  (ivy-rich-mode 1))
+
+;; ivy-xref - select from xref candidates with ivy
+;; https://github.com/alexmurray/ivy-xref
+;;
+(use-package ivy-xref
+  :ensure t
+  :init (if (< emacs-major-version 27)
+            (setq xref-show-xrefs-function #'ivy-xref-show-xrefs)
+          (setq xref-show-definitions-function #'ivy-xref-show-defs)))
+
+;; COUNSEL
+;;---------
+(use-package counsel :ensure t
+  :bind*
+  (("M-x"     . counsel-M-x)
+   ("C-s"     . swiper)
+   ("C-x C-f" . counsel-find-file)
+   ("C-x C-r" . counsel-recentf)   ; search for recently edited
+   ("C-c c"   . counsel-compile)
+   ("C-c g"   . counsel-git)       ; search for files in git repo
+   ("C-c j"   . counsel-git-grep)  ; search for regexp in git repo
+   ("C-c /"   . counsel-ag)        ; Use ag for regexp
+   ("C-x l"   . counsel-locate)
+   ("<f1> f"  . counsel-describe-function)
+   ("<f1> v"  . counsel-describe-variable)
+   ("<f1> l"  . counsel-find-library)
+;   ("<f2> i"  . counsel-info-lookup-symbol)
+;   ("<f2> u"  . counsel-unicode-char)
+   ("C-c C-r" . ivy-resume)        ; Resume last Ivy-based completion
+   ("C-c o"   . counsel-find-file-extern)
+   )
+  :config
+  (setq counsel-find-file-at-point t)
+  (setq counsel-locate-cmd 'counsel-locate-cmd-default)
+  (setq counsel-find-file-ignore-regexp "\\.DS_Store\\|.git"))
+
+(use-package counsel-projectile
+  :ensure t
+  :config
+  (counsel-projectile-mode))
