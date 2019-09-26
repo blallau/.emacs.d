0ac95e968bb8e7060f4e33eca4e1c15997479f60
T0125936 - LALLAU Bertrand <bertrand.lallau@thalesgroup.com>
2019-09-26 18:36:49 +0200

    Modify smart shift to use use-package

--- package-config/smart-shift.el
+++ package-config/smart-shift.el
@@ -1,2 +1,8 @@
-(require 'smart-shift)
-(global-smart-shift-mode 1)
+;; make it easy to shift current line or region to
+;; left/right according to current major mode indentation.
+(use-package smart-shift
+  :bind
+  ("C-c <right>" . smart-shift-right)
+  ("C-c <left>" . smart-shift-left)
+  :config
+  (advice-add 'smart-shift-override-local-map :override #'ignore))
