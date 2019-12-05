84375f255d646a9d1e33a5e3f66003e9feb9f661
T0125936 - LALLAU Bertrand <bertrand.lallau@thalesgroup.com>
2019-12-05 15:33:34 +0100

    Simplify iedit code

--- package-config/iedit.el
+++ package-config/iedit.el
@@ -1,19 +1,2 @@
-(use-package iedit
-  :defer t
-  :config
-  (defun iedit-dwim (arg)
-    "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
-    (interactive "P")
-    (if arg
-        (iedit-mode)
-      (save-excursion
-        (save-restriction
-          (widen)
-          ;; this function determines the scope of `iedit-start'.
-          (if iedit-mode
-              (iedit-done)
-            ;; `current-word' can of course be replaced by other
-            ;; functions.
-            (narrow-to-defun)
-            (iedit-start (current-word) (point-min) (point-max)))))))
-  :bind ("C-;" . iedit-dwin))
+(use-package iedit                                      ;
+  :defer t)
